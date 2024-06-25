01d0fa963c0a3fc1385a1be4eb8e1680684f5009
Isaias Ramos <isaias-paz@msn.com>
2024-06-25 10:14:48 -0700

    Add messages and Room model

---
+++ app/Models/Room.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Room extends Model
+{
+    use HasFactory;
+
+    public $table = 'room';
+    protected $fillable = ['id', 'admin_id', 'name'];
+
+    
+    public function admin(): BelongsTo {
+        return $this->belongsTo(User::class, 'admin_id');
+    }
+}
--- database/migrations/2024_06_25_170101_create_rooms_table.php
+++ database/migrations/2024_06_25_170101_create_rooms_table.php
@@ -11,11 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('messages', function (Blueprint $table) {
+        Schema::create('rooms', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->constrained();
-            $table->foreignId('room_id')->constrained();
-            $table->text('text')->nullable();
+            $table->foreignId('admin_id')->constrained();
+            $table->text('name');
             $table->timestamps();
         });
     }
@@ -25,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('messages');
+        Schema::dropIfExists('rooms');
     }
 };

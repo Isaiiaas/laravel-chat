d8c4f2d4c94ef2ecb41f457fdbd3cacc1fdf8eb8
Isaias Ramos <isaias-paz@msn.com>
2024-06-25 10:14:53 -0700

    Update Routes

--- routes/web.php
+++ routes/web.php
@@ -10,11 +10,13 @@ Route::get('/', function () {
 
 //Restricted Routes
 Route::middleware(['auth'])->group(function () {
-    Route::get('dashboard', [DashboardController::class, 'dashboardView'])->middleware('auth');
+    Route::get('/dashboard', [DashboardController::class, 'dashboardView']);
     // Rooms
-    // Route::get('room/view', [RoomsController::class, 'dashboard']); 
-    // Route::post('messageSend', [RoomsController::class, 'dashboard']); 
-    // Route::post('messageDelete', [RoomsController::class, 'dashboard']); 
+    // Route::get('rooms', [RoomsController::class, 'dashboard']);
+    // Route::put('rooms', [RoomsController::class, 'dashboard']);
+    // Messages: 
+    // Route::get('/messages/{roomId}', [RoomsController::class, 'dashboard'])->whereUuid('id');
+    // Route::put('/messages', [RoomsController::class, 'dashboard']); 
 });
 
 // Route::middleware(['auth','adminAuth'])->group(function () {
@@ -24,12 +26,12 @@ Route::middleware(['auth'])->group(function () {
 // });
 
 Route::controller(AuthController::class)->group(function () {
-    Route::get('login', 'loginView')->name('login');
-    Route::post('login','userLogin')->name('userLogin');
+    Route::get('/login', 'loginView')->name('login');
+    Route::post('/login','userLogin')->name('userLogin');
     
-    Route::get('register', 'registrationView')->name('register-user');
-    Route::post('register', 'userRegister')->name('userRegister');
+    Route::get('/register', 'registrationView')->name('register-user');
+    Route::post('/register', 'userRegister')->name('userRegister');
     
-    Route::get('signout', 'signOut')->name('signout');
+    Route::get('/signout', 'signOut')->name('signout');
 });
 
